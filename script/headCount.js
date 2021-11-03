862300ed9a546087e33a14c1a5c3aaaabbacd85c
Nickid2018 <nickid2018@outlook.com>
2021-11-04 00:19:36 +0800

    Site updated: 2021-11-04 00:19:36

--- script/headCount.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?f32d0139ad5f3b55317728550c14d428";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
